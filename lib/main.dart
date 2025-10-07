da14fd593edb808d39ba42514817ae7bac9ce25d
Farhanrezy <farhanrezy70@gmail.com>
2025-10-07 19:42:22 +0700

    method Column _buildButtonColumn

--- lib/main.dart
+++ lib/main.dart
@@ -5,28 +5,6 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // ✅ Langkah 1: Buat method _buildButtonColumn
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color), // ikon sesuai warna primer
-        Container(
-          margin: const EdgeInsets.only(top: 8), // jarak antara ikon dan teks
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color, // warna teks sama dengan warna ikon
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     Widget titleSection = Container(
@@ -70,6 +48,27 @@ class MyApp extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color), // ikon sesuai warna primer
+        Container(
+          margin: const EdgeInsets.only(top: 8), // jarak antara ikon dan teks
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color, // warna teks sama dengan warna ikon
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
